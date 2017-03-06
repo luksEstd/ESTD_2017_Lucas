c99894a13336b0e49bef44bcb02317128872ea52
lalves <lalves@LAB-B121-13.univille.br>
2017-03-06 20:35:54 -0300

    commit

---
+++ src/estd/Array.java
@@ -0,0 +1,14 @@
+package estd;
+
+import java.util.Arrays;
+
+public class Array {
+	int[] i = { 5, 6, 7 } ;
+	Integer[] y;
+	
+	public void test(){
+	    System.out.println(Arrays.toString(i));	
+	}
+	
+
+}
